7740d7e031a5bb74cad578199ed8a0954a80e839
Obi Abii <coa22@cornell.edu>
2023-01-04 14:37:56 -0500

    create user and recipe structures

---
+++ structs/user.js
@@ -0,0 +1,19 @@
+
+const DEFAULT = {
+  user_id: 'default',
+  recipe_list: []
+}
+
+export function makeUser(id, recipe_list) {
+  return {
+    user_id: id,
+    recipe_list: recipe_list
+  }
+}
+
+export function addRecipeToUser(user, recipe) {
+  return {
+    user_id: user.user_id,
+    recipe_list: user.recipe_list.concat(recipe)
+  }
+}
