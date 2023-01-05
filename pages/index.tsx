52d12b33b4629fba2d5ad4514619dcddb0112495
Obi Abii <coa22@cornell.edu>
2023-01-05 15:13:43 -0500

    convert to TypeScript - restructure db class, moved it out of api

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from "./index.module.css";
 import RecipeCard from "../components/Recipe/RecipeCard";
 import Button from "../components/Button/Button";
 import { useUser } from '@auth0/nextjs-auth0/client';
+import React from "react";
 
 
 export default function Home() {
