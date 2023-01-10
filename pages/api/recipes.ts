2edb97b1559b75463505e66a8e1d732fcf6dbe49
Thomas Lu <16872741+thomaslu3@users.noreply.github.com>
2023-01-10 15:30:21 -0700

    Added base file for recipes endpoints

---
+++ pages/api/recipes.ts
@@ -0,0 +1,66 @@
+import { ddbDocClient, TABLE_NAME } from "../../db/db";
+import { makeRecipeObject, Recipe } from "../../interfaces/recipe";
+import { PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
+
+function makePostParams(recipes: Recipe[]) {
+  return {
+    TableName: TABLE_NAME,
+    Item: recipes
+  };
+}
+
+function makeGetParams(user_id: string) {
+  return {
+    TableName: TABLE_NAME,
+    Key: {
+      user_id: user_id,
+    },
+  };
+}
+
+async function post(user: User, recipes: Recipe[], res) {
+  let getParams = makeGetParams(user.user_id);
+  const result = await ddbDocClient.send(new GetCommand(getParams));
+  if (result.Item !== undefined && result.Item !== null) {
+    res.status(200).json("USER EXISTS");
+  }
+  // need to get users object, update it, and re-post it
+
+  let postParams = makePostParams(recipes);
+  await ddbDocClient.send(new PutCommand(postParams));
+  res.status(200).json("SUCCESS");
+}
+
+async function get(user_id: string, res) {
+  let params = makeGetParams(user_id);
+  const data = await ddbDocClient.send(new GetCommand(params));
+  res.status(200).json(data.Item);
+}
+
+async function handler(req, res) {
+  try {
+    let sesh = await getSession(req, res);
+    if (sesh == null) {
+      throw new Error("User logged out");
+    }
+
+    let user_id: string = sesh.user.sub;
+
+    if (req.method == "POST") {
+      let user = makeUser(user_id, []);
+      post(user, res);
+    }
+
+    if (req.method == "GET") {
+      get(user_id, res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(error.status || 500).json({
+      error: error,
+    });
+  }
+}
+
+export default withApiAuthRequired(handler);
